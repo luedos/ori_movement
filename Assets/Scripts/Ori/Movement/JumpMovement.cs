7e479520667d66b44ff76ee85a8561060f744f56
luedos <vl.ol@mail.ru>
2019-10-23 19:21:48 -0700

    Added high jump option

--- Assets/Scripts/Ori/Movement/JumpMovement.cs
+++ Assets/Scripts/Ori/Movement/JumpMovement.cs
@@ -9,9 +9,11 @@ public class JumpMovement : MovementComponent
 	// Public members.
 	//
 	//! Height of the jump.
-	public float jumpHeight = 4.0f;
+	public float lowJumpHeight = 4.0f;
 	//! Time till jump apex.
-	public float jumpApexTime = 0.4f;
+	public float lowJumpApexTime = 0.4f;
+	//! Height of the jump.
+	public float highJumpHeight = 6.0f;
 	//! Possible number of jumps.
 	public uint jumpsNumber = 1;
 
@@ -22,16 +24,26 @@ public class JumpMovement : MovementComponent
 	BaseMovement baseMovement;
 	//! Velocity for jump.
 	float jumpVelocity;
-	//! Jumps count.
+	//! Jumps counter.
 	[HideInInspector]
 	public uint jumps = 0;
-	
+	//! Low jump gravity.
+	float lowJumpGravity;
+	//! High jump gravity.
+	float highJumpGravity;
+	//! Time till jump apex.
+	float highJumpApexTime;
+	//! Is player currently jumping.
+	bool jumping = false;
+
 	//
 	// Public interface.
 	//
 	//! Makes jump.
 	public void Jump(float xVelocity = 0.0f)
 	{
+		jumping = true;
+		baseMovement.gravity = highJumpGravity;
 		baseMovement.velocity.y = jumpVelocity;
 		baseMovement.velocity.x += xVelocity;
 	}
@@ -42,8 +54,12 @@ public class JumpMovement : MovementComponent
 	//! Resets all gravity parameters.
 	void ResetGravityParams()
 	{
-		baseMovement.gravity = -2 * jumpHeight / (jumpApexTime * jumpApexTime);
-		jumpVelocity = -baseMovement.gravity * jumpApexTime;
+		lowJumpGravity = -2 * lowJumpHeight / (lowJumpApexTime * lowJumpApexTime);
+		jumpVelocity = -lowJumpGravity * lowJumpApexTime;
+		highJumpApexTime = 2 * highJumpHeight / jumpVelocity;
+		highJumpGravity = -jumpVelocity / highJumpApexTime;
+
+		baseMovement.gravity = lowJumpGravity;
 	}
 	// Start is called before the first frame update
 	protected override void Start()
@@ -56,7 +72,22 @@ public class JumpMovement : MovementComponent
     // Update is called once per frame
     void Update()
     {
-		if ((controller.GetCollisionState() & Controller2D.CollisionState.COLLIDE_BELOW) != 0)
+		Controller2D.CollisionState state = controller.GetCollisionState();
+
+		if (jumping)
+		{
+			if (state != 0)
+			{
+				jumping = false;
+			}
+
+			if (!jumping || baseMovement.velocity.y < 0 || !Input.GetButton("B_J"))
+			{
+				baseMovement.gravity = lowJumpGravity;
+			}
+		}
+
+		if ((state & Controller2D.CollisionState.COLLIDE_BELOW) != 0)
 		{
 			jumps = jumpsNumber;
 		}
